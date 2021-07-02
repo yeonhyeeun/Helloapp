4f5f20480bf3abaaa1da2c53bee27f733d4b2814
YeonHyeEun <21900442@handong.edu>
2021-07-02 10:14:16 +0900

    create hw3

--- app/src/main/java/com/example/helloworld/firstactivity.java
+++ app/src/main/java/com/example/helloworld/firstactivity.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class firstactivity extends AppCompatActivity {
-    Button b1, b2;
-    EditText etvm;
+    Button b1, b2, b3;
+    EditText e1, e2;
+    TextView tv1;
+
+    private final static int REQUEST_CODE_1 =1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +31,56 @@ public class firstactivity extends AppCompatActivity {
             }
         });
 
+
+
         b2 = findViewById(R.id.bu2);
+        e1 = findViewById(R.id.etvm);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CallSecondActivityWithMsg();
             }
         });
-        etvm=findViewById(R.id.etvm);
+
+        b3 = findViewById(R.id.bu3); //버튼 동작
+        e2 = findViewById(R.id.etvm2);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CallSecondActivityWithMsg3(); // 새로운 함수 생성
+            }
+        });
+
+    }
+
+    private void CallSecondActivityWithMsg3() {
+        Intent intent = new Intent(this, secondact.class); //private 함수여서 this 사용 가능
+        intent.putExtra("msg", e2.getText().toString());
+        startActivityForResult(intent, REQUEST_CODE_1); //requestcode도 같이 보냄
     }
 
     private void CallSecondActivityWithMsg() {
-            Intent intent = new Intent(this, secondact.class);
-            intent.putExtra("msg", etvm.getText().toString());
-            startActivity(intent);
-            }
+        Intent intent = new Intent(this, secondact.class);
+        intent.putExtra("msg", e1.getText().toString());
+        startActivity(intent);
+    }
 
 
     private void CallSecondActivity(){
-            Intent intent = new Intent(this, secondact.class); //private 함수여서 this 사용 가능
-            startActivity(intent);
+        Intent intent = new Intent(this, secondact.class); //private 함수여서 this 사용 가능
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_1) {
+            if(resultCode == RESULT_OK) {
+                String msg = data.getStringExtra("msg"); //데이타 받아오기
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
             }
+        }
+    }
 }
--- app/src/main/java/com/example/helloworld/secondact.java
+++ app/src/main/java/com/example/helloworld/secondact.java
@@ -6,40 +6,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class secondact extends AppCompatActivity {
 
-    Button b1;
+    Button b2_1, b2_2;
     TextView tv1;
+    EditText et1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondact);
 
-        b1 = findViewById(R.id.bu2_1); //버튼 동작
-        b1.setOnClickListener(new View.OnClickListener() {
+        b2_1 = findViewById(R.id.bu2_1); //버튼 동작
+        b2_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 callBack(); //
             }
         });
 
-    tv1 = findViewById(R.id.tvm);
-    Intent intent = getIntent();  //앞에서 보낸 인탠트 데이터를 가져오는 것
+        tv1 = findViewById(R.id.tvm);
+        Intent intent = getIntent();  //앞에서 보낸 인탠트 데이터를 가져오는 것
         //데이터 타입 일치시키기 - string
         //받을 문자열 변수 선언하기
         //getextra로 못 받음 - 파라미터 일치
         String msg = intent.getStringExtra("msg"); //앞에서 보낸 이름이랑 같아야 함
         tv1.setText(msg);
 
+        b2_2 = findViewById(R.id.bu2_2); //버튼 동작
+        b2_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CallSendMsg(); //
+            }
+        });
+        et1=findViewById(R.id.etmsg);
+
     }
 
+
+    private void CallSendMsg() {
+        Intent intent = new Intent(this, firstactivity.class);
+        intent.putExtra("msg", et1.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
+    }
 
     private void callBack() { //자기 자신 액티비티만 종료해서 첫번째 액티비티만 보이게 함
         finish(); //현재 보여지고 있는 두번째 액티티비를 종료시킴
     }
-
-
 }
